6302a1452571c04ede1f07127b16a7df6a9bb7fc
managne <agne.manaikaite@gmail.com>
2023-05-10 12:38:03 +0300

    feat: Understanding Typescript  Stop 29. The "unknown" Type

--- app.ts
+++ app.ts
@@ -1,24 +1,9 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
-};
+let userInput: unknown; // more strict than type 'any'
+let userName: string;
 
-function printResult(num: number): void {
-    console.log('Result: ' + num);
-};
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-};
-
-printResult(add(5, 12));
-
-let combinedValues: (a: number, b: number) => number;
-
-combinedValues = add;
-
-console.log(combinedValues(8, 8))
-
-addAndHandle(10, 20, (result) => {
-    console.log(result)
-})
+userInput = 5;
+userInput = 'Max';
+// userName = userInput; ERROR!!!
+if(typeof userInput === 'string'){
+    userName = userInput;
+}
